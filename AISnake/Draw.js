c2330f27203b82dd4f027585c1383c1691165368
StarikTenger <StarikTenger@gmail.com>
2022-08-13 12:15:30 +0300

    draw js refactor methods

--- AISnake/Draw.js
+++ AISnake/Draw.js
@@ -7,53 +7,53 @@ class Draw {
        this.cam = new Vec2(0, 0); // Camera position
        this.center = new Vec2(32, 27); // Screen center (здфнукы ы)
     }
-}
 
-Draw.prototype.image = function(texture, x, y, w, h, flip) {
-    x = Math.round(x);
-    y = Math.round(y);
-    w = Math.round(w);
-    h = Math.round(h);
-
-    if(!flip)
-        flip = 0;
-        
-    this.ctx.save();
-    let width = 1;
-    if (flip) {
-        this.ctx.scale(-1, 1);
-        width = -1;
-    }
-    this.ctx.imageSmoothingEnabled = 0;
-    this.ctx.drawImage(texture, width*(x + w * flip - this.cam.x + this.center.x) * SCALE, (y - this.cam.y + this.center.y) * SCALE, w * SCALE, h * SCALE);
-    this.ctx.restore();
-};
-
-Draw.prototype.rect = function(x, y, w, h, color) {
-    x = Math.round(x);
-    y = Math.round(y);
-    w = Math.round(w);
-    h = Math.round(h);
-
-    this.ctx.imageSmoothingEnabled = 0;
-    this.ctx.fillStyle = color;
-    this.ctx.fillRect((x - this.cam.x + this.center.x) * SCALE, (y - this.cam.y + this.center.y) * SCALE, w * SCALE, h * SCALE);
-};
-
-Draw.prototype.draw = function(game) {
-
-    // Focusing camera
-    this.cam = game.player.pos;
-    this.center = new Vec2(32, 27);
-
-    // Filling background
-    this.ctx.fillStyle = "black";
-    this.ctx.fillRect(0, 0, 10000, 10000);
-
-    // Grid
-    for (let x = 0; x < SIZE_X; x++) {
-        for (let y = 0; y < SIZE_Y; y++) {
+    image(texture, x, y, w, h, flip) {
+        x = Math.round(x);
+        y = Math.round(y);
+        w = Math.round(w);
+        h = Math.round(h);
+    
+        if(!flip)
+            flip = 0;
             
+        this.ctx.save();
+        let width = 1;
+        if (flip) {
+            this.ctx.scale(-1, 1);
+            width = -1;
         }
-    }    
-};
+        this.ctx.imageSmoothingEnabled = 0;
+        this.ctx.drawImage(texture, width*(x + w * flip - this.cam.x + this.center.x) * SCALE, (y - this.cam.y + this.center.y) * SCALE, w * SCALE, h * SCALE);
+        this.ctx.restore();
+    }
+
+
+    rect(x, y, w, h, color) {
+        x = Math.round(x);
+        y = Math.round(y);
+        w = Math.round(w);
+        h = Math.round(h);
+    
+        this.ctx.imageSmoothingEnabled = 0;
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect((x - this.cam.x + this.center.x) * SCALE, (y - this.cam.y + this.center.y) * SCALE, w * SCALE, h * SCALE);
+    }
+
+    draw(game) {
+        // Focusing camera
+        this.cam = game.player.pos;
+        this.center = new Vec2(32, 27);
+    
+        // Filling background
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(0, 0, 10000, 10000);
+    
+        // Grid
+        for (let x = 0; x < SIZE_X; x++) {
+            for (let y = 0; y < SIZE_Y; y++) {
+                
+            }
+        }    
+    };
+}
